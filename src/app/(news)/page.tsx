ae6a4fff159af7972147ad72aee682e2f8cd233a
Okera Johnson <okerajohnson@gmail.com>
2025-08-05 15:13:52 -0400

    okay lets see

--- src/app/(news)/page.tsx
+++ src/app/(news)/page.tsx
@@ -1,2 +1,12 @@
 import React from "react";
+import { PostDataType } from "@/data/types";
 
+import SectionLargeSlider from "@/app/(home)/SectionLargeSlider";
+
+const PageNews = () => {
+  return (
+    <div>
+      <SectionLargeSlider />
+    </div>
+  );
+};
